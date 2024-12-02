ce74a205e4dfdd27ac982e1a6841a00907feadfe
Anne <anne.vc2008@aluno.ifsc.edu.br>
2024-12-02 13:01:26 -0300

    Aula 02 12 2024

---
+++ 20241202/main.py
@@ -0,0 +1,13 @@
+#coding: utf-8 
+
+import requests, json
+def RequisicaoBasica():
+    url = "https://www.mercadobitcoin.net/api/BTC/trades/1686731660/1686733260/"
+    requisicao = requests.get(url)
+    print(requisicao)
+    lista1 = requisicao.json()
+    print(lista1)
+    print("Quantidade de dados: ", len(lista1))
+
+if __name__=="__main__":
+    RequisicaoBasica()
