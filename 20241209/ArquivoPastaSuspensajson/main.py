c4864f7a5a8cadf5cd2f0f375be4ec458ec31150
Anne <anne.vc2008@aluno.ifsc.edu.br>
2024-12-09 12:45:30 -0300

    aula do dia 09 de dezembro de 2024

---
+++ 20241209/ArquivoPastaSuspensajson/main.py
@@ -0,0 +1,35 @@
+#coding: utf-8
+
+from ClassePasta import *
+from ClasseArquivoPastaSuspensa import *
+import time as t 
+
+def CriarPasta(QdeArquivos=5): 	
+	Pasta1 = Pasta() #Crio a pasta
+	Pasta1.CriarPastaVazia()	
+	for _ in range(QdeArquivos):		
+		Pasta1.Receber_arquivo()
+	Pasta1.get_chaves_arquivos_pasta()
+	return Pasta1				
+		 	
+def CriarArquivo(): 
+	Arq1 = ArquivoPastaSuspensa("amarelo", "Pesquisa",NumMaxPastas=20)		
+	return Arq1
+
+def MostrarPastas(Pastas):
+	for i, j in enumerate(Pastas):
+		print(f"Pasta (i) com conteudo(j.conteudo) ")
+		for c, k in j.documento.items():
+			print(f"\t chave (c) e valor (k)")
+		
+
+if __name__ == "__main__": 
+    ArquivosDePastas = []
+    NumPastasCriadas = 2 
+    for _ in range(NumPastasCriadas):
+        PastaSuspensa = CriarPasta()
+        ArquivosDePastas.append(PastaSuspensa)
+        t.sleep(1.2) #O programa vai parar por 1.2 segundos para mudar o momento unix
+        print("Mudanca de pasta")
+        MostrarPastas(ArquivosDePastas)
+        print()
