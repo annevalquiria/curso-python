d3b886e2ece6adbc7b387a471dae9af51117c34b
Anne <anne.vc2008@aluno.ifsc.edu.br>
2025-02-05 13:07:16 -0300

    aula do dia 05 de fevereiro de 2025

---
+++ 20250205/PalhetaCores.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+
+#Color Picker
+
+#Cores usadas
+CorBranco = "#feffff"
+CorRoxa = '#cca2eb'
+CorLaranja = "#ffab40"
+CorCinzaClaro = "#c9c7c9"
+CorCinzaEscuro = "#6e6d6e"
+CorRosa = "#e01675"
